a0fde3251acc9c5fe73e8c49425f65be717904db
Kai Jukarainen <kai.jukarainen@gmail.com>
2022-10-25 10:38:18 +0300

    Registeration & LogIn popUp

--- main.js
+++ main.js
@@ -9,3 +9,29 @@ menu.addEventListener('click', () => {
 close.addEventListener('click', () => {
     nav.classList.remove('open-nav')
 });
+
+// RegisterationPopUp //
+const registeration = document.getElementById('rekisteroidu')
+const closePop = document.getElementById('closeForm')
+const regPopUp = document.querySelector('.formContainer')
+
+registeration.addEventListener('click', () => {
+    regPopUp.classList.add('open-registerationPopUp')
+    nav.classList.remove('open-nav')
+});
+closePop.addEventListener('click', () => {
+    regPopUp.classList.remove('open-registerationPopUp')
+});
+
+// LogInPopUp //
+const logIn = document.getElementById('kirjaudu')
+const closeLogInPop = document.getElementById('close2Form')
+const LogInPopUp = document.querySelector('.form2Container')
+
+logIn.addEventListener('click', () => {
+    LogInPopUp.classList.add('open-logInPopUp')
+    nav.classList.remove('open-nav')
+});
+    closeLogInPop.addEventListener('click', () => {
+    LogInPopUp.classList.remove('open-logInPopUp')
+});
